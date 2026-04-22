2d5e9f3e4b3ceae249f2cd7fc663eb14d141b3e7
Juan David Villanueva Rojas <jdavidvillanueva@ucompensar.edu.co>
2026-04-22 15:30:05 -0500

    Introduccion a SQLite3

---
+++ db.js
@@ -0,0 +1,22 @@
+const sqlite3 = require("sqlite").verbose();
+
+//Crea una base de datos nueva 
+const db = new sqlite3.Database("./jwt.db", (err) => {
+    //Si hay error al conectar la base de datos muestreme error, si no muestreme un mensaje conctando a la bd 
+    if (err) console.error(err.message)
+        console.log("Conectando a la base de datos SQLite")
+})
+
+//Con el comando run 
+db.serialize(() => {
+    db.run(`CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT UNIQUE NOT NULL,
+            password TEXT NOT NULL,
+            name TEXT NOT NULL,
+            created_at DATETIME DEFAULT CURRENT_TIMESTAMP      
+        )`
+    )
+})
+
+module.exports = db
